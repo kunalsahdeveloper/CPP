f23dc0a13e20d2611b2fc24909c63a811ad7b0ca
Kunal Sah <indkunalsah534@gmail.com>
2025-01-29 23:20:20 +0530

    updated mergesort

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,43 +1,77 @@
 #include<iostream>
 using namespace std;
 
-void merge(int arr[], int s, int mid, int e) {
-    int n1 = mid - s + 1;
-    int n2 = e - mid;
+// void merge(int arr[], int s, int mid, int e) {
+//     int n1 = mid - s + 1;
+//     int n2 = e - mid;
 
-    int left[n1], right[n2];
+//     int left[n1], right[n2];
 
-    for(int i = 0; i < n1; i++)
-        left[i] = arr[s + i];
-    for(int j = 0; j < n2; j++)
-        right[j] = arr[mid + 1 + j];
+//     for(int i = 0; i < n1; i++)
+//         left[i] = arr[s + i];
+//     for(int j = 0; j < n2; j++)
+//         right[j] = arr[mid + 1 + j];
 
-    int i = 0, j = 0, k = s;
-    while(i < n1 && j < n2) {
-        if(left[i] <= right[j]) {
-            arr[k] = left[i];
+//     int i = 0, j = 0, k = s;
+//     while(i < n1 && j < n2) {
+//         if(left[i] <= right[j]) {
+//             arr[k] = left[i];
+//             i++;
+//         } else {
+//             arr[k] = right[j];
+//             j++;
+//         }
+//         k++;
+//     }
+
+//     while(i < n1) {
+//         arr[k] = left[i];
+//         i++;
+//         k++;
+//     }
+
+//     while(j < n2) {
+//         arr[k] = right[j];
+//         j++;
+//         k++;
+//     }
+// }
+
+
+void merge(vector<int> &arr, int st, int mid, int end){
+
+    vector<int> temp;
+    int i = st, j = mid+1;
+
+    while(i <= mid && j <= end){
+        if(arr[i] <= arr[j]){
+            temp.push_back(arr[i]);
             i++;
-        } else {
-            arr[k] = right[j];
+        }else{
+            temp.push_back(arr[j]);
             j++;
         }
-        k++;
     }
-
-    while(i < n1) {
-        arr[k] = left[i];
+    while(i <= mid){
+        temp.push_back(arr[i]);
         i++;
-        k++;
+    }
+    while (j <= end){
+        temp.push_back(arr[j]);
+        j++;
     }
 
-    while(j < n2) {
-        arr[k] = right[j];
-        j++;
-        k++;
+    for(int idx=0; idx < temp.size(); idx++){
+        arr[idx + st] = temp[idx];
     }
+    
 }
 
-void mergeSort(int arr[], int s, int e) {
+
+
+
+
+void mergeSort(vector<int> &arr, int s, int e) {
     if(s < e) {
         int mid = s + (e - s) / 2;
 
@@ -52,7 +86,7 @@ int main() {
     cout << "Enter the number of elements: ";
     cin >> n;
 
-    int arr[n];
+    vector<int> arr(n);
     cout << "Enter the elements: \n";
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
