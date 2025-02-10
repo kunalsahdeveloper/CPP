d163328aa4eba34c2368504b7387bff093b3796f
Kunal Sah <indkunalsah534@gmail.com>
2025-02-10 10:37:23 +0530

    10 feb

---
+++ LeetCode/876MiddleofLinkedList.cpp
@@ -0,0 +1,29 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* middleNode(ListNode* head) {
+    
+            ListNode *s = head;
+            ListNode *f = head;
+    
+            while(f && f->next){
+                s = s->next;
+                f = f->next->next;
+    
+            }
+           return s;
+            
+        }
+    };
